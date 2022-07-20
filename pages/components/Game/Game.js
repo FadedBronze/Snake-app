db0b3a8bfc66ba5dc71f0ccb2c0b0e0f4cdd7bd7
FadedBronze <TheSamyatGautam@gmail.com>
2022-07-20 19:17:02 -0400

    some refactors and improvements

--- pages/components/Game/Game.js
+++ pages/components/Game/Game.js
@@ -2,51 +2,82 @@ import { useEffect, useRef, useState } from 'react'
 
 import styles from '../../../styles/Game.module.css'
 
+const tileData = []
+const snakeTileIndexs = []
+
 const Game = ({ wn, canvasSize }) => {
     const c = useRef(null)
-    const tileData = []
-    const gridSize = 20
-    const tileSize = canvasSize / gridSize
+
+    const gridSize = 15
+    const tileSize = Math.ceil(canvasSize / gridSize)
     const canvasColor = "rgb(10, 10, 10)"
+    const snakeColor = "green"
+    const snakeLength = 10
+    const snakeStartPos = {x: 0, y: 0}
 
     const updateTiles = () => {
         tileData.map(tile => {
             const ctx = c.current.getContext('2d')
             ctx.fillStyle = tile.color
-            ctx.fillRect(tile.x * tileSize + 1, tile.y * tileSize + 1, tileSize - 2, tileSize - 2)
+            ctx.fillRect(tile.x * tileSize, tile.y * tileSize, tileSize, tileSize)
         })
     }
 
     const addTile = (tile) => {
+        
+        let replaced = tileData.findIndex(
+            oldtile => 
+            oldtile.x == tile.x &&
+            oldtile.y == tile.y
+        )
+
+        console.log(replaced, tile)
+
         tileData.splice(
-            tileData.findIndex(_tile => _tile.x == tile.x && _tile.y == tile.y), 
+            replaced, 
             1,
             tile
         )
+
+        return replaced
     }
 
     useEffect(() => {
+        if (tileData.length > 0) return
+
+        //setting up game tiles
+    
         console.log(wn)
-        
+    
         for (let i = 0; i < gridSize; i++) {
             for (let j = 0; j < gridSize; j++) {
                 tileData.push({ x: j, y: i, type: "empty", color: canvasColor })
             }
         }
 
-        addTile({ x: 9, y: 9, type: "apple", color: "red" })
-        addTile({ x: 8, y: 8, type: "apple", color: "red" })
-        addTile({ x: 7, y: 7, type: "apple", color: "red" })
-        addTile({ x: 6, y: 6, type: "apple", color: "red" })
-        addTile({ x: 5, y: 5, type: "apple", color: "red" })
-        addTile({ x: 4, y: 4, type: "apple", color: "red" })
-        addTile({ x: 3, y: 3, type: "apple", color: "red" })
-        addTile({ x: 2, y: 2, type: "apple", color: "red" })
-        addTile({ x: 1, y: 1, type: "apple", color: "red" })
-        addTile({ x: 0, y: 0, type: "apple", color: "red" })
+        //creating snake tiles
+
+        for (let i = 0; i < snakeLength; i++) {
+            snakeTileIndexs.push(
+                addTile({ x: i + snakeStartPos.x, y: snakeStartPos.y, type: "snake", color: snakeColor })
+            )
+        }
 
         console.log(tileData)
+
         updateTiles()
+
+        //
+
+        setInterval(() => {
+            console.log('ran')
+        }, 70)
+    }, [])
+
+    useEffect(() => {
+        updateTiles()
+
+        console.log(tileData)
     }, [wn])
 
     return ( 
